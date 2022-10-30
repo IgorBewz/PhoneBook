da6c1ed4970cac03f4a921c31ea87318ba85e129
bohdan-voit <113583224+bohdan-voit@users.noreply.github.com>
2022-10-30 09:29:51 +0200

    First try

---
+++ Phone_book_rev/src/main/java/Team_project/phonebook/Storage.java
@@ -0,0 +1,12 @@
+package Team_project.phonebook;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Storage {
+    Contact save(Contact contact);
+    Contact remove(int id);
+    Optional<Contact> find(int id);
+    List<Contact> find();
+    List<Contact> find(String keyword);
+}
